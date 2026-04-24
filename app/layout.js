05a5926b25a1fa471f7c539b6145acc40ded7470
Mecharhythm <dhzyrswwzw@privaterelay.appleid.com>
2026-04-24 18:14:43 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  export default function RootLayout({ children }) {
 }) {
   return (
     <html lang="ja">
